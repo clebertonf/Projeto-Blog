5b0b5a5f11dbd1c0d235bc1441ed97445846a32d
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-08 12:14:46 -0300

    Estruturando padrão MVC

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const { PORT } = process.env;
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('./src/public'));
 
 app.get('/', (_req, resp) => {
   resp.render('index');
