ca09348bb9329ffcca74e4b44638986f191815b9
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-10 11:50:54 -0300

    Adicionando erro de categoria na view na view

--- src/controllers/CategoriesController.js
+++ src/controllers/CategoriesController.js
@@ -1,13 +1,19 @@
 const CategoriesModel = require('../models/CategoriesModel');
 
 const renderViewAddCategorie = (_req, resp) => {
-  resp.render('categories/newCategorie');
+  const error = false;
+  resp.render('categories/newCategorie', { error });
 };
 
 const addCategorieBank = async (req, resp) => {
   const { title } = req.body;
+  const error = 'Aconteceu algo de errado ao cadastrar sua Categoria!';
   const response = await CategoriesModel.addCategorieBank(title);
-  if (response) resp.redirect('/admin/categories/new');
+  if (response) {
+    resp.redirect('/admin/categories/new');
+  } else {
+    resp.render('categories/newCategorie', { error });
+  }
 };
 
 module.exports = {
