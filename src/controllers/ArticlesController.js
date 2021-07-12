2fd36b71ac25095bea6ea32eed20bc6deb80fd7b
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-12 12:45:40 -0300

    Ajustando eslint

--- src/controllers/ArticlesController.js
+++ src/controllers/ArticlesController.js
@@ -2,31 +2,31 @@ const CategoriesModel = require('../models/CategoriesModel');
 const ArticlesModel = require('../models/ArticlesModel');
 
 const renderViewAddArticle = async (req, resp) => {
-    const response = await CategoriesModel.listCategoriesBank();
-    resp.render('articles/newArticle', { response })
-}
+  const response = await CategoriesModel.listCategoriesBank();
+  resp.render('articles/newArticle', { response });
+};
 
 const createArticle = async (req, resp) => {
-    const { title, article, id_categorie } = req.body;
-    const response = await ArticlesModel.addArticleBank(title, article, id_categorie);
-    if(response) resp.redirect('/admin/articles/list');
-}
+  const { title, article, id_categorie } = req.body;
+  const response = await ArticlesModel.addArticleBank(title, article, id_categorie);
+  if (response) resp.redirect('/admin/articles/list');
+};
 
 const listArticles = async (_req, resp) => {
-    const response = await ArticlesModel.listArticleBank();
-    if (response) return resp.render('articles/listArticles',{ response });
-    resp.redirect('/articles/list');
-}
+  const response = await ArticlesModel.listArticleBank();
+  if (response) return resp.render('articles/listArticles', { response });
+  resp.redirect('/articles/list');
+};
 
 const deleteArticle = async (req, resp) => {
-    const { id } = req.body;
-    const response = await ArticlesModel.deleteArticleBank(id);
-    if(response) resp.redirect('/admin/articles/list');
-}
+  const { id } = req.body;
+  const response = await ArticlesModel.deleteArticleBank(id);
+  if (response) resp.redirect('/admin/articles/list');
+};
 
 module.exports = {
-    renderViewAddArticle,
-    createArticle,
-    listArticles,
-    deleteArticle
-}
+  renderViewAddArticle,
+  createArticle,
+  listArticles,
+  deleteArticle,
+};
--- src/models/ArticlesModel.js
+++ src/models/ArticlesModel.js
@@ -2,31 +2,30 @@ const slugify = require('slugify');
 const connection = require('./connection');
 
 const addArticleBank = async (title, article, id_categorie) => {
-    const slugifyTitle = slugify(title);
-    const query = 'INSERT INTO blog.tb_article (title, article, slug, id_categorie) VALUES (?, ?, ?, ?)';
-    const response = await connection.execute(query, [title, article, slugifyTitle, id_categorie]);
-    return response;
+  const slugifyTitle = slugify(title);
+  const query = 'INSERT INTO blog.tb_article (title, article, slug, id_categorie) VALUES (?, ?, ?, ?)';
+  const response = await connection.execute(query, [title, article, slugifyTitle, id_categorie]);
+  return response;
 };
 
 const listArticleBank = async () => {
-    const query = 'SELECT b.id_article, b.title, b.slug, a.title as title_categorie FROM tb_categorie as a INNER JOIN tb_article as b USING(id_categorie)';
-    const [response] = await connection.execute(query);
-    return response;
+  const query = 'SELECT b.id_article, b.title, b.slug, a.title as title_categorie FROM tb_categorie as a INNER JOIN tb_article as b USING(id_categorie)';
+  const [response] = await connection.execute(query);
+  return response;
 };
 
 const deleteArticleBank = (id) => {
-    try {
-        const query = 'DELETE FROM blog.tb_article WHERE id_article = ?';
-        const response = connection.execute(query, [id]);
-        return response;
-        
-      } catch (err) {
-        console.error(err);
-      }
-}
+  try {
+    const query = 'DELETE FROM blog.tb_article WHERE id_article = ?';
+    const response = connection.execute(query, [id]);
+    return response;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 module.exports = {
-    addArticleBank,
-    listArticleBank,
-    deleteArticleBank
-}
+  addArticleBank,
+  listArticleBank,
+  deleteArticleBank,
+};
