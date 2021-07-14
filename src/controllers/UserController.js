1baeb8b96f71b42efbb35abb648c2ce53d780ec2
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-14 17:56:29 -0300

    Finalizando cadastro user no banco e evitando emails duplicados

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,13 +1,27 @@
+const bcrypt = require('bcryptjs');
+const UserModel = require('../models/UserModel');
+
 const listUsers = (req, resp) => {
   resp.send('Listar usuarios');
 };
 
 const viewCreateUser = (req, resp) => {
-  resp.render('user/createUser');
+  const error = false;
+  resp.render('user/createUser', { error });
 };
 
-const createUser = (req, resp) => {
+const createUser = async (req, resp) => {
   const { name, email, password } = req.body;
+  const error = 'Email informado ja existe';
+
+  const userExists = await UserModel.verifyUserBank(email);
+  if (userExists.length >= 1) return resp.render('user/createUser', { error });
+
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
+
+  const response = await UserModel.createNewUserBank(name, email, hash);
+  if (response) resp.redirect('/');
 };
 
 module.exports = {
---
+++ src/models/UserModel.js
@@ -0,0 +1,26 @@
+const connection = require('./connection');
+
+const createNewUserBank = async (name, email, hash) => {
+  try {
+    const query = 'INSERT INTO blog.tb_user (name, email, password) VALUES (?, ?, ?)';
+    const response = await connection.execute(query, [name, email, hash]);
+    return response;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const verifyUserBank = async (email) => {
+  try {
+    const query = 'SELECT * FROM blog.tb_user WHERE email = ?';
+    const [response] = await connection.execute(query, [email]);
+    return response;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+module.exports = {
+  createNewUserBank,
+  verifyUserBank,
+};
