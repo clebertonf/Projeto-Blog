aa14f1c5790c9247b456357e9ea4e4b9259e6c55
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-16 21:34:11 -0300

    Adicionando Try catch na função listArticlesByCategoryBank

--- src/models/IndexModel.js
+++ src/models/IndexModel.js
@@ -21,9 +21,13 @@ const listArticleIDBank = async (slug) => {
 };
 
 const listArticlesByCategoryBank = async (id) => {
-  const query = 'SELECT b.title, b.slug, b.article, b.preview, b.created_at, a.title as title_categorie FROM blog.tb_categorie as a INNER JOIN blog.tb_article as b USING(id_categorie) WHERE a.id_categorie = ?';
-  const [response] = await connection.execute(query, [id]);
-  return response;
+  try {
+    const query = 'SELECT b.title, b.slug, b.article, b.preview, b.created_at, a.title as title_categorie FROM blog.tb_categorie as a INNER JOIN blog.tb_article as b USING(id_categorie) WHERE a.id_categorie = ?';
+    const [response] = await connection.execute(query, [id]);
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const listArticlesNumBank = async (num) => {
