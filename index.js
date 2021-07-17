e66df00a4a85645d25e9f49410ebbb67aaed892b
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-16 21:28:32 -0300

    Try catch Function addArticleBank

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(session({
   },
 
 }));
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('./src/public'));
--- src/models/ArticlesModel.js
+++ src/models/ArticlesModel.js
@@ -2,11 +2,15 @@ const slugify = require('slugify');
 const connection = require('./connection');
 
 const addArticleBank = async (title, preview, article, idCategorie) => {
-  const slugifyTitle = slugify(title);
-  const query = 'INSERT INTO blog.tb_article (title, article, preview, slug, id_categorie) VALUES (?, ?, ?, ?, ?)';
-  const response = await connection
-    .execute(query, [title, article, preview, slugifyTitle, idCategorie]);
-  return response;
+  try {
+    const slugifyTitle = slugify(title);
+    const query = 'INSERT INTO blog.tb_article (title, article, preview, slug, id_categorie) VALUES (?, ?, ?, ?, ?)';
+    const response = await connection
+      .execute(query, [title, article, preview, slugifyTitle, idCategorie]);
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const listArticleBank = async () => {
